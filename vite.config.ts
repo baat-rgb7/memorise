7030d00d446855a7ba32fb01a281a25b1335f84d
baat-rgb7 <allainbaptiste24@gmail.com>
2026-02-14 16:48:27 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-          base: '/memorise/',
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(AIzaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8),
-        'process.env.GEMINI_API_KEY': JSON.stringify(AIzaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8),
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/memorise/',
+  define: {
+    'process.env.API_KEY': JSON.stringify("AizaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8"),
+    'process.env.GEMINI_API_KEY': JSON.stringify("AizaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8")
+  }
+})
