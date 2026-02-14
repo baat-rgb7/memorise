047ec5e46b4b62cdb40c944d0ec302e816de3e7a
baat-rgb7 <allainbaptiste24@gmail.com>
2026-02-14 16:51:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/memorisation/',
+  base: '/memorise/',
   define: {
     'process.env.API_KEY': JSON.stringify("AizaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8"),
     'process.env.GEMINI_API_KEY': JSON.stringify("AizaSyDUZcyzLfl-e_xHyzKIDE1kwSUPT0Ibws8")
